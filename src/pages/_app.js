67369f295b9d43e2b648e5d671e46b9d2cff83e9
Raheel7 <38526220+Raheel7@users.noreply.github.com>
2023-02-20 23:26:49 +0500

    Update _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -4,7 +4,7 @@ import SubHeader from "../components/SubHeader";
 import AppContext from "../data/AppContext";
 import { store } from "../data/store";
 import Image from "next/image";
-import styles from "../styles/main.module.scss";
+import styles from "../styles/Main.module.scss";
 import "./../styles/globals.scss";
 import Sidebar from "@/components/Sidebar";
 
